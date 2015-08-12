7a963c57bde4cc20ee9e20f8acf9ead607a233e7
Carryl Sorene <carrylsorene@gmail.com>
2015-08-12 18:01:37 +0100

    basic and advanced conditional

--- calc.js
+++ calc.js
@@ -1,30 +1,56 @@
+var calc = prompt('Please type B to use the Basic calculator or A for Advanced');
+  console.log('calc', calc);
+
+  if (calc === "B") {
+
+var firstNum = prompt('choose the first number');
+  console.log('firstNum', firstNum);
+
 var op = prompt('choose an operation');
 console.log('op', op);
 
-var num = prompt('choose a number');
+var secondNum= prompt('choose the second number');
+  console.log('secondNum', secondNum);
 
-num = parseFloat(num);
-
-console.log('num', num);
+firstNum = parseFloat(firstNum);
+secondNum = parseFloat(secondNum);
 
 /*alert("Why is your console not open?");*/
 
 
 if (op === "+") { 
-  console.log(num + num);
+  console.log(firstNum + secondNum);
 }
 else if (op === "-") {
-  console.log(num - num);
+  console.log(firstnNum - secondNum);
 
 }
 else if (op === "*") {
-  console.log(num * num);
+  console.log(firstNum * secondNum);
 }
 
 else if (op === "/") {
-  console.log(num / num);
+  console.log(firstNum / secondNum);
 }
 
-if (num === NaN) {
+if (firstNum === NaN && secondNum === NaN) {
   alert("That is not a number. Please try again");
 }
+}
+
+ else if (calc === 'A') {
+
+var op = prompt('choose an operation');
+console.log('op', op);
+
+var Num= prompt('choose the number');
+  console.log('Num', Num);
+ }
+
+ if (op === ) /*power*/ {
+
+}
+  else if 
+    (op ===) /*sq root*/ {
+
+  }
