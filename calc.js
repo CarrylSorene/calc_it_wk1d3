5ac60f2ad545aa731c82e405822ba40d78d59c95
Carryl Sorene <carrylsorene@gmail.com>
2015-08-12 19:14:36 +0100

    Fixed or statements and added answer alerts

--- calc.js
+++ calc.js
@@ -1,75 +1,71 @@
 var calc = prompt('Please type B to use the Basic calculator or A for Advanced');
   console.log('calc', calc);
 
-  if (calc === "B") {
+  if ( (calc === "B") || (calc === "b") ) {
+    console.log('Okay')
+    var firstNum = prompt('choose the first number');
+    console.log('firstNum', firstNum);
 
-var firstNum = prompt('choose the first number');
-  console.log('firstNum', firstNum);
+    var op = prompt('choose an operation');
+    console.log('op', op);
 
-var op = prompt('choose an operation');
-console.log('op', op);
+    var secondNum= prompt('choose the second number');
+    console.log('secondNum', secondNum);
 
-var secondNum= prompt('choose the second number');
-  console.log('secondNum', secondNum);
-
-  firstNum = parseFloat(firstNum);
-  secondNum = parseFloat(secondNum);
+    firstNum = parseFloat(firstNum);
+    secondNum = parseFloat(secondNum);
 
 /*alert("Why is your console not open?");*/
 
-  if (op === "+") { 
-  console.log(firstNum + secondNum);
-  /*alert("firstNum + secondNum");*/
+      if (op === "+") { 
+        console.log(firstNum + secondNum);
+         var answer = (firstNum + secondNum);
+         alert("The answer is " + answer);
+      }
+
+      else if (op === "-") {
+        console.log(firstNum - secondNum);
+        var answer = (firstNum - secondNum);
+        alert("The answer is " + answer);
+      }
+
+      else if (op === "*") {
+        console.log(firstNum * secondNum);
+        var answer = (firstNum * secondNum);
+        alert("The answer is " + answer);
+      }
+
+      else if (op === "/") {
+        console.log(firstNum / secondNum);
+        var answer = (firstNum / secondNum);
+        alert("The answer is " + answer);
+      }
+
+      else if (firstNum === NaN && secondNum === NaN) {
+        alert("That is not a number. Please try again");
+      }
   }
 
-  else if (op === "-") {
-  console.log(firstNum - secondNum);
-  /*alert("firstNum - secondNum")*/
-  }
-
-  else if (op === "*") {
-  console.log(firstNum * secondNum);
-  /*alert("firstNum * secondNum");*/
-  }
-
-  else if (op === "/") {
-  console.log(firstNum / secondNum);
-  /*alert(firstNum / secondNum);*/
-  }
-
-  if (firstNum === NaN && secondNum === NaN) {
-  alert("That is not a number. Please try again");
-  }
-}
-
-  else if (calc === 'A') {
-
-var op = prompt('Please type power or square root');
-console.log('op', op);
-
-  if (op === "power")  {
-
-  var firstNum = prompt('choose the first number');
-  console.log('firstNum', firstNum);
+  if ((calc === "A") || (calc === "a")) {
+    var op = prompt('Please type P for power or S for square root');
+    console.log('op', op);
+      if (op === "P" || "p")  {
+        var firstNum = prompt('choose the first number');
+        console.log('firstNum', firstNum);
   
-  var secondNum= prompt('choose the second number');
-  console.log('secondNum', secondNum);
-}
+        var secondNum = prompt('choose the second number');
+        console.log('secondNum', secondNum);
 
-  if (secondNum === 2) {
-  Math.pow(firstNum * (firstNum * firstNum));
-}
-
-  else if (secondNum === 3) {
-    Math.pow(firstNum * (firstNum * firstNum * firstNum))
+        var answer = Math.pow(firstNum, secondNum);
+        alert("The answer is " + answer);
   }
 }
 
-  else if 
-    (op === "square root") {
+//   else if 
+//     (op === "S" || "s") {
   
-var Num= prompt('choose the number');
-console.log('Num', Num);
+// var Num = prompt('choose the number');
+// console.log('Num', Num);
 
-  Math.sqrt(Num / Num);
-}
+//   /*Math.sqrt(Num "/" Num);*/
+// }
